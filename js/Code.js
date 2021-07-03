b1df439a9268037de95e48e2b2ace29062e7f244
2006Aryan <76159948+2006Aryan@users.noreply.github.com>
2021-07-03 16:50:04 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,26 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white");
+    textSize(15);
+    text(" N U C T I F O N", 700,120);
+    fill("lightblue");
+    text("Hint: Defines what we do !!", 700,150);
+
+    fill("white");
+    textSize(15);
+    text("T A B S D A A E", 350,120);
+    fill("lightblue");
+    text("Hint: Stores Information", 350,150);
+    
+
+}
